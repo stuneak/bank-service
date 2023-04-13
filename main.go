22af5141009efad041038fea613b9fe4f7df5c06
stuneak <stuneak@gmail.com>
2023-04-13 13:20:53 +0300

    env config

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 
 	"github.com/stuneak/simplebank/api"
 	db "github.com/stuneak/simplebank/db/sqlc"
+	"github.com/stuneak/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8088"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the db", err)
 	}
@@ -25,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server", err)
